f1a6abb4637532e6847cb2f8dc20e63362c9cced
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-01-28 11:33:55 +0000

    Added comments to Lab 1 code

--- Lab 1/src/fibonacci.c
+++ Lab 1/src/fibonacci.c
@@ -2,13 +2,20 @@
 
 int main() {
     puts("----- FIBONACCI -----");
-    int n;
-    printf("n > "); scanf("%d", &n);
 
+    // Input an integer to the variable "n".
+    // Note that we pass &n to scanf, this is because the function takes a pointer
+    // as an argument, and places the input from the user into the variable using its pointer.
+    int n;
+    printf("n > "); 
+    scanf("%d", &n);
+
+    // Check if n is 0, 1, or 2, and handle those special cases accordingly
     if (n == 0) return 0;
     else if (n == 1) { puts("0"); return 0; }
     else if (n == 2) { puts("0\n1"); return 0; }
 
+    // Calculate the Fibonacci sequence up to n, this will only be reached if n is not in (0, 1, 2).
     int a, b; a = 0; b = 1;
     puts("0\n1");
 
@@ -20,5 +27,6 @@ int main() {
         b = c;
     }
 
+    // Return 0 to the caller
     return 0;
 }
--- Lab 1/src/prime.c
+++ Lab 1/src/prime.c
@@ -2,16 +2,21 @@
 
 int main() {
     puts("----- PRIME -----");
+    // Input a value n
     int n;
     printf("n > ");
     scanf("%d", &n);
 
+    // Booleans are not a standard type in C, instead we can use an integer with the value of 0 or 1
     int is_prime = 1;
 
+    // Check if n is even, if so, it is not prime (i.e. if the least signifcant bit of n is 1, it is odd)
     if ((n & 1) == 0) {
         is_prime = 0;
     }
 
+    // If n is odd, then check if n is divisible by any numbers from 3 -> (n - 1),
+    // if so, n is not prime
     if (is_prime) {
         for (int i = 3; i < n; i++) {
             if ((n % i) == 0) {
@@ -21,10 +26,14 @@ int main() {
         }
     }
 
+    // Output result accordingly
     if (is_prime) {
         printf("%d is prime.", n);
     }
     else {
         printf("%d is not prime.", n);
     }
+
+    // Return 0 to caller
+    return 0;
 }
